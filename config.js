602d411ece314b392583b44f28532ef601f65941
wsws4567890 <infnix766@gmail.com>
2025-06-27 18:09:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.nEUxBj7DIYRZNuuhR7Zd_BcVlI8yDLagEZxXO7zUQj4,  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
